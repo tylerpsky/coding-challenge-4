f3866ba00a174e87e48e191d27bae7df9738d7a1
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-02 23:30:09 -0500

    Initial commit - Control Flow and Functions Challenge Setup.

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Task 1
+
+let purchaseAmount = 150; // Set the purchase amount
+// Apply a 10% discount if the purchase amount is greater than $100
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.9;
+}
+// Log the final amount to the console
+console.log(`Final amount after discount: $${finalAmount}`);
